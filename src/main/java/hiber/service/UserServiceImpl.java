70136b7223bd5d5efe0f507fad3af7257f51f1ec
vyacheslavkalyakin <kalyakinvvv@mail.ru>
2025-07-18 14:35:32 +0300

    Fix

--- src/main/java/hiber/service/UserServiceImpl.java
+++ src/main/java/hiber/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
@@ -21,32 +22,38 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
-    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
     @Override
-    @Transactional
     public void deleteUser(int id) {
         userDao.deleteUser(id);
     }
 
     @Override
-    @Transactional
     public User getUserById(int id) {
         return userDao.getUserById(id);
+    }
+
+    @Override
+    public void editUser(int id, String username, String password, String email) {
+        User user = userDao.getUserById(id);
+        if (user != null) {
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setEmail(email);
+            userDao.updateUser(user);
+        }
     }
 }
